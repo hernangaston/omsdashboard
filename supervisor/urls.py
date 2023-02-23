1f8f18f32feb17b986d41fd717b2c69ced595ae5
Abril <86980732+Abril9@users.noreply.github.com>
2023-02-22 23:32:16 -0300

    endpoints: maquinas, oprs, ratio a json

--- supervisor/urls.py
+++ supervisor/urls.py
@@ -1,9 +1,20 @@
-from django.contrib.auth.decorators import login_required
 from django.urls import path
-from .views import ListaDeMaquinas, ListaTotalDeMaquinas, lista_de_maquinas
+from .views import *
 
 urlpatterns = [
     path('lista_maquinas/', ListaDeMaquinas.as_view(), name='lista-maquinas'),
     path('listado_datos_maquinas/', ListaTotalDeMaquinas.as_view(), name='lista-maquinas'),
-    path('listado_datos_json/', lista_de_maquinas, name='lista-maquinas')
+    
+
+    #paths para recuperar datos en formato json 
+    path('maquinas_json/', maquinas_json, name='maquinas-json'),
+    path('maquina_opr_json/<int:id>', maquina_opr_json, name='maquina-opr-json'),
+    
+    path('oprs_json/', oprs_json, name='oprs-json'),
+    path('opr_json/<int:id>', opr_json, name='opr-json'),
+    
+    path('operarios_json/', operarios_json, name='operarios-json'),
+
+    path('ratio_mes_json/', ratio_mes_json, name='ratio_mes_json'),
+    
 ]

cf07057ab9c9e2fea5248c8e37ed043bc0c268d2
root <hernangaston@gmail.com>
2023-03-28 14:02:21 +0000

    commit reiniciado

--- omsdashboard/settings.py
+++ omsdashboard/settings.py
@@ -67,9 +67,9 @@ MIDDLEWARE = [
 
 CORS_ALLOW_ALL_ORIGINS = True
 #CORS_ORIGIN_ALLOW_ALL = True
-'''CORS_ORIGIN_WHITELIST = (
-  'http://localhost:8000',
-)'''
+CORS_ORIGIN_WHITELIST = (
+  'http://localhost:8000','https://omas-alpha.vercel.app'
+)
 
 ROOT_URLCONF = 'omsdashboard.urls'
 

30e57692cab24b033e88dab4c613da805d90d002
Hernan Gaston <hernangaston@gmail.com>
2023-03-30 16:39:06 -0300

    fix redirect a home

--- omsdashboard/urls.py
+++ omsdashboard/urls.py
@@ -20,5 +20,6 @@ from .views import index
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
+    path('home', index, name='index'),
     path('supervisor/', include('supervisor.urls'))
 ]
